6646741bd4aa127ce3688d449fc78cc1971d2ae3
Suman Cheon <167512644+smc5224@users.noreply.github.com>
2024-09-03 23:29:12 +0900

    Add files via upload

---
+++ 천수만+김정우/deco.js
@@ -0,0 +1,40 @@
+document.querySelector('.hat1').addEventListener('click', function() {
+    alert('밀짚 모자가 착용되었습니다.');
+    // function hat1Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.hat2').addEventListener('click', function() {
+    alert('캡 모자가 착용되었습니다.');
+        // function hat2Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.hat3').addEventListener('click', function() {
+    alert('사과 모자가 착용되었습니다.');
+        // function hat3Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.hat4').addEventListener('click', function() {
+    alert('귤 모자가 착용되었습니다.');
+        // function hat4Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.hat5').addEventListener('click', function() {
+    alert('왕관 모자가 착용되었습니다.');
+        // function hat5Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.reset-button').addEventListener('click', function() {
+    alert('초기화 버튼이 클릭되었습니다.');
+        // function resetImg() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
+
+document.querySelector('.apply-button').addEventListener('click', function() {
+    alert('적용 버튼이 클릭되었습니다.');
+});
+
---
+++ 천수만+김정우/write.js
@@ -0,0 +1,21 @@
+function loadFile(input) {
+    let file = input.files[0]; // 선택파일 가져오기
+
+    let newImage = document.createElement("img"); //새 이미지 태그 생성
+
+    //이미지 source 가져오기
+    newImage.src = URL.createObjectURL(file);
+    newImage.style.width = "100%"; //div에 꽉차게 넣으려고
+    newImage.style.height = "100%";
+    newImage.style.objectFit = "cover"; // div에 넘치지 않고 들어가게
+
+    //이미지를 image-show div에 추가
+    let container = document.getElementById('image-show');
+    container.appendChild(newImage);
+}
+
+document.querySelector('.upload-btn').addEventListener('click', function() {
+    alert('글이 업로드되었습니다.');
+        // function hat2Img() {
+    //     document.getElementById("img").src = "./images/rose.jpg";
+});
